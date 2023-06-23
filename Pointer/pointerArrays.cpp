0874e56b933ff2539c59b244a2574e2be5be8124
Abhishek Tiwari <Abhishektiwari2083@gmail.com>
2023-06-23 23:09:09 +0530

    new addition

--- Pointer/pointerArrays.cpp
+++ Pointer/pointerArrays.cpp
@@ -1,9 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
- int main()
+int main()
 {
-    int arr[10]={0};
-    cout << "the address of first memory block is:- ";
-    cout<<endl;
+    int arr[10] = {23, 122, 41, 67};
+    cout << "the address of first memory block is:- " << arr << endl;
+    cout << "the address of first memory block is:- " << &arr[0] << endl;
+    cout << "testing *arr::- " << *(arr + 2) <<endl;
+
+
+    int i = 0;
+    cout << "line:-" << i[arr];
     return 0;
 }
