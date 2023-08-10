9c4137cb28ea5645896dcae2c7e6458ffa8868ec
Abhishek Tiwari <Abhishektiwari2083@gmail.com>
2023-08-10 23:42:34 +0530

    updates

--- Pointer/pointerArrays.cpp
+++ Pointer/pointerArrays.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 int main()
 {
-    int arr[10] = {23, 122, 41, 67};
-    cout << "the address of first memory block is:- " << arr << endl;
-    cout << "the address of first memory block is:- " << &arr[0] << endl;
-    cout << "testing *arr::- " << *(arr + 2) <<endl;
+    // int arr[10] = {23, 122, 41, 67};
+    // cout << "the address of first memory block is:- " << arr << endl;
+    // cout << "the address of first memory block is:- " << &arr[0] << endl;
+    // cout << "testing *arr::- " << *(arr + 2) <<endl;
 
 
-    int i = 0;
-    cout << "line:-" << i[arr];
+    // int i = 0;
+    // cout << "line:-" << i[arr];
+
+
+    int array[10]={1};
+    int *p = &array[0];
+    cout << p<< endl; 
+    cout << *p<< endl;
+    cout << &p<< endl;
+
     return 0;
-}
+ }
+
