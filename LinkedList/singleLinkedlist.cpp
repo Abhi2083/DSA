7bbfe6b3fac49aaa9466a1710f47bd5cd85bbb40
Abhishek Tiwari <Abhishektiwari2083@gmail.com>
2023-12-17 09:33:37 +0530

    delete function added in singly linedlist

--- LinkedList/singleLinkedlist.cpp
+++ LinkedList/singleLinkedlist.cpp
@@ -28,11 +28,12 @@ void insertAtTail(LinkedlistNode *&tail, int d)
     tail = temp;
 }
 
-void insertAtPosition(LinkedlistNode *&head, LinkedlistNode* &tail, int position, int d)
+void insertAtPosition(LinkedlistNode *&head, LinkedlistNode *&tail, int position, int d)
 {
     // insert at start
-    if(position == 1){
-        insertAtHead(head,d);
+    if (position == 1)
+    {
+        insertAtHead(head, d);
         return;
     }
     LinkedlistNode *temp = head;
@@ -43,17 +44,55 @@ void insertAtPosition(LinkedlistNode *&head, LinkedlistNode* &tail, int position
         cnt++;
     }
 
-// inserting at last position
-    if(temp->next == NULL){
-        insertAtTail(tail,d);
+    // inserting at last position
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
         return;
     }
     LinkedlistNode *nodeToInsert = new LinkedlistNode(d);
     nodeToInsert->next = temp->next;
     temp->next = nodeToInsert;
-
 }
+void deleteNode(LinkedlistNode *&head, int position)
+{
+    // Check if the given position is valid
+    int listLength = 0;
+    LinkedlistNode* temp = head;
+    while (temp != NULL)
+    {
+        listLength++;
+        temp = temp->next;
+    }
 
+    if (position < 0 || position > listLength)
+    {
+        cout << "Invalid position. Please provide a position within the range of the linked list." << endl;
+        return;
+    }
+    // deleting at 1st pos
+    if (position == 0)
+    {
+        LinkedlistNode *temp = head;
+        head = head->next;
+        delete temp;
+    }
+    else
+    { // deleting at any pos
+        LinkedlistNode* curr = head;
+        LinkedlistNode* prev = NULL;
+        int cnt = 1;
+        while(cnt <= position){
+            prev = curr;
+            curr = curr ->next;
+            cnt++;
+        }
+
+        prev->next= curr->next;
+        delete curr;
+
+    }
+}
 void print(LinkedlistNode *&head)
 {
     LinkedlistNode *temp = head;
@@ -96,8 +135,11 @@ int main()
 
     // insertAtTail(tail, 90);
     // print(head);
-    // print(tail);    
+    // print(tail);
 
-    insertAtPosition(head,tail, 6, 7);
+    insertAtPosition(head, tail, 6, 7);
     print(head);
+
+    deleteNode(head,0);
+    print(head); 
 }
